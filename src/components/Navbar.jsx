6c92bc2344a3d1f57c353804e8fb74cda3a61406
Angelik4 <angelikcu4@gmail.com>
2025-08-28 20:58:22 -0500

    Ajuste login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <img src="https://www.tuboleta.com/themes/custom/tu_boleta_theme/logo.png" alt="Logo tuboleta" />
-            <div className="navbar__logo">
-                Task<span className="navbar__highlight">Manager</span>
-            </div>
-        </nav>
-    );
+export default function Navbar({ user, onLogout }) {
+  return (
+    <nav className="navbar">
+      <img
+        src="https://www.tuboleta.com/themes/custom/tu_boleta_theme/logo.png"
+        alt="Logo tuboleta"
+      />
+      <div className="navbar__logo">
+        Task<span className="navbar__highlight">Manager</span>
+      </div>
+      {user && (
+        <button className="navbar__btnLogin" onClick={onLogout}>
+          Cerrar sesión
+        </button>
+      )}
+    </nav>
+  );
 }
